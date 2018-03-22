39d11fde8f0bdb4e5adfd3ac595d80f186d42b08
peiht <1071532941@qq.com>
2018-03-23 01:04:29 +0800

    first commit

---
+++ src/main/java/com/myspring/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.myspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+	@RequestMapping("/index")
+	public String index(){
+		return "demo";
+	}
+}
